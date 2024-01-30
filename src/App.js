e5822547b674cde69ebb9e0453df430d5362784a
Antoine ROYER BOURNELLE <royerb.antoine@gmail.com>
2024-01-30 15:35:59 +0100

    Add GitHub and LinkedIn logos to footer

--- src/App.js
+++ src/App.js
@@ -171,7 +171,7 @@ export default function Game() {
 
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move, paragraph)}>{description}</button>
+        <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
   });
